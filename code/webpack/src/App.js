7a1f0554aca71dff2ee151a8a4386f6fe4a6c84f
urvashi29 <urvashigolu29@gmail.com>
2023-02-22 10:29:05 +0530

    added webpack config

--- code/webpack/src/App.js
+++ code/webpack/src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
+import data from "./data";
 
 const App = () => {
+  console.log(data.recipes);
+
+  const recipeList = data.recipes.map((recipe) => {
     return (
-        <div>
-            Hello
-        </div>
+      <React.Fragment key={recipe.idCategory}>
+        <img src={recipe.strCategoryThumb} />
+        <p>{recipe.strCategory}</p>
+        <p>{recipe.strCategoryDescription.slice(0, 10)}</p>
+      </React.Fragment>
     );
+  });
+
+  return <div>{recipeList}</div>;
 };
 
 export default App;
